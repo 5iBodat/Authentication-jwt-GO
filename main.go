1670176451804173cf3ee369e7427e6545be7b3e
Sahat <sahat.marbun@merkleinnovation.co.id>
2024-10-14 19:55:08 +0700

    change log to fmt

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"authentication-jwt-go/src/router"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	log.Println("Server starting at http://localhost:%s", os.Getenv("PORT"))
+	fmt.Printf("Server starting at http://localhost:%s", os.Getenv("PORT"))
 	r := router.Router()
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
